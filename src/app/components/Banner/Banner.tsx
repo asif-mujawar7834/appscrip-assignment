4ae4218417e72d873d57d41326f0e1bcc3b78eaa
Asif Mujawar <asiflpnmujawar@gmail.com>
2024-08-22 07:17:27 +0530

    added mobile navbar indicator

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -2,6 +2,11 @@ import styles from "./Banner.module.css";
 export const Banner = () => {
   return (
     <section className={styles.banner_container}>
+      <div className={styles.mobile__nav__indicator}>
+        <span>Home</span>
+        <span></span>
+        <span>Shop</span>
+      </div>
       <h1>DISCOVER OUR PRODUCTS</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
